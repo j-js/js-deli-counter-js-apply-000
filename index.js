67978502ff560678b90cf1a0521d7940b68f9845
Hyojin Yoo <yoohyojin@gmail.com>
2020-02-25 04:23:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ function takeANumber(katzDeliLine, name){
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
-function nowServing
+function nowServing(katzDeliLine){
+  
+  if(katzDeliLine.length){
+    return katzDeliLine[0];
+  }else{
+    return "The line is ";
+  }
+}
