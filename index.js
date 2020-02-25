1532aa5e63d13ad74aec47affd38234ef903707e
Hyojin Yoo <yoohyojin@gmail.com>
2020-02-25 04:26:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ function takeANumber(katzDeliLine, name){
 function nowServing(katzDeliLine){
   
   if(katzDeliLine.length){
-    return katzDeliLine[0];
     katzDeliLine.shift();
+    return katzDeliLine[0];
+    
   }else{
     return "There is nobody waiting to be served!";
   }
